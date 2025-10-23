81d4c365ebc2a372c74375b4b876d529acc52720
socalledtheraven <cassie.dalrymple3@gmail.com>
2025-10-23 14:30:57 +0100

    get the player moving

--- Game/core/src/main/java/io/github/some_example_name/Main.java
+++ Game/core/src/main/java/io/github/some_example_name/Main.java
@@ -1,8 +1,11 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -13,13 +16,17 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 public class Main extends Game {
     SpriteBatch batch;
     FitViewport viewport;
-    Texture playerSprite;
+    Sprite player;
+    Texture background;
 
     @Override
     public void create() {
         setScreen(new MapManager(this));
 
-        playerSprite = new Texture("Art/Characters/Main Character/Character_Idle.png");
+        // both temporary until we have a proper background and player sprite
+        player = new Sprite(new Texture("Art/Characters/Main Character/Character_Idle.png"));
+        player.setSize(1, 1);
+        background = new Texture("Art/Buildings/House_Hay_3.png");
 
         batch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
@@ -32,14 +39,34 @@ public class Main extends Game {
 
     @Override
     public void render() {
-        // organize code into three methods
+        // organise code into three methods
         input();
         logic();
         draw();
     }
 
     private void input() {
+        // libgdx wants floats for everything
+        float speed = 4f;
+        // delta time to avoid speed discrepancies with different framerates
+        float delta = Gdx.graphics.getDeltaTime();
 
+        // separated if statements so we can move in multiple directions at once
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            player.translateX(speed * delta);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            player.translateX(-speed * delta);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            player.translateY(speed * delta);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            player.translateY(-speed * delta);
+        }
     }
 
     private void logic() {
@@ -47,14 +74,16 @@ public class Main extends Game {
     }
 
     private void draw() {
-        // clears the screen - should be done every frame, or we get graphical errors
+        // clears the screen - should be done every frame, or we get "graphical errors"
         ScreenUtils.clear(Color.BLACK);
+
         // boilerplate
         viewport.apply();
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
 
-        batch.draw(playerSprite, 0, 0, 1, 1);
+        batch.draw(background, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight()); // draw the background
+        player.draw(batch);
 
         batch.end();
     }
